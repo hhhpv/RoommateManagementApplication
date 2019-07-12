85152a3589bb595e10b344c905c46eea8bf90153
Acadivity <acadivity@gmail.com>
2019-07-12 17:00:39 +0530

    signup skeleton

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:roomate/UI/home.dart';
-
+import 'package:roomate/UI/signup.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: new Home(),
+      home: new SignUpForm(),
     );
   }
 }
